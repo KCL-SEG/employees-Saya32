17e5b75015fcf1d7584015c8f40af6d5c85c3535
Sayaka Bhandari <sayakabhandari@gmail.com>
2022-12-08 21:23:23 +0000

    None Assert error

--- employee.py
+++ employee.py
@@ -14,11 +14,12 @@ class Employee:
             pay += self.salary
         if self.contract_hours and self.pay_rate:
             pay += self.contract_hours * self.pay_rate
-        if self.commission_contracts and self.pay_rate:
-            pay += self.commission_contracts * self.pay_rate
+        if self.commission_contracts and self.commision_rate:
+            pay += self.commission_contracts * self.commision_rate
         if self.bonus_commission:
             pay += self.bonus_commission
         return pay
+
 
     def __str__(self):
         s = f'{self.name} works on '
@@ -26,10 +27,8 @@ class Employee:
             s += f'a monthly salary of {self.salary}'
         if self.contract_hours and self.pay_rate:
             s += f'a contract of {self.contract_hours} hours at {self.pay_rate}/hour'
-        if self.commission_contracts and self.pay_rate:
-            s += f'a commission for {self.commission_contracts} contract(s) at {self.pay_rate}/contract'
         if self.commission_contracts and self.commision_rate:
-            s += f'a commission for {self.commission_contracts} contract(s) at {self.pay_rate}/contract'
+            s += f'a commission for {self.commission_contracts} contract(s) at {self.commision_rate}/contract'
         if self.bonus_commission:
             s += f'receives a bonus commission of {self.bonus_commission}'
         s += f'\nTheir total pay is {self.get_pay()}.'
@@ -42,7 +41,7 @@ billie = Employee('Billie', salary=4000)
 charlie = Employee('Charlie', pay_rate=25, contract_hours=100)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee', pay_rate=200, salary=3000, commission_contracts=4)
+renee = Employee('Renee', pay_rate=0, salary=3000, commission_contracts=4, commision_rate=200)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = Employee('Jan', pay_rate=25, contract_hours=150, commission_contracts=3, commision_rate= 220)
